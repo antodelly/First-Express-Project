933d3c5a428f397731d32143fb35187fae878d73
antodelly <a.dellifiori997@gmail.com>
2025-07-15 11:26:34 +0200

    Aggiunta una piccola API di tipo GET

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ const app = express();
 dotenv.config();
 
 app.use(express.json);
-app.listen(process.env.PORT, () =>
-  console.log(`SERVER STARTED ON htp://localhost:${process.env.PORT}`)
-);
+app.listen(process.env.PORT, () => {
+  console.clear();
+  console.log("======================================");
+  console.log(`SERVER STARTED ON htp://localhost:${process.env.PORT}`);
+  console.log("======================================");
+});
+
+// Database connection
+/**
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.error("MongoDB connection error:", err));
+*/
+
+// Example route
+app.get("/ping", (req, res) => {
+  res.send("Hello World!");
+});
