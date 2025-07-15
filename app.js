813ed28b29f285ae5c89fb3da9d576e94aef7477
antodelly <a.dellifiori997@gmail.com>
2025-07-15 11:18:45 +0200

    Aggiunta prima gestione con la lib dotenv

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
 dotenv.config();
 
 app.use(express.json);
-app.listen(process.env.PORT, () => console.log(`SERVER STARTED ON htp://localhost:${PORT}`))
-
+app.listen(process.env.PORT, () =>
+  console.log(`SERVER STARTED ON htp://localhost:${process.env.PORT}`)
+);
