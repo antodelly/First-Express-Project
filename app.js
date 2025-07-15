56d83ffe6284b64287d72658bbc51e8b6f40a580
antodelly <a.dellifiori997@gmail.com>
2025-07-15 15:24:31 +0200

    Implementate connessione a DB Locale con MongoDB

--- app.js
+++ app.js
@@ -5,23 +5,24 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config();
 
-app.use(express.json);
+app.use(express.json());
 app.listen(process.env.PORT, () => {
   console.clear();
   console.log("======================================");
-  console.log(`SERVER STARTED ON htp://localhost:${process.env.PORT}`);
+  console.log(`SERVER STARTED ON http://localhost:${process.env.PORT}`);
   console.log("======================================");
 });
 
 // Database connection
-/**
 mongoose
-.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB connected"))
-.catch((err) => console.error("MongoDB connection error:", err));
-*/
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log(`MongoDB connected. DB used is ${process.env.MONGO_DB_NAME}`);
+    console.log("======================================");
+  })
+  .catch((err) => console.error("MongoDB connection error: ", err));
 
 // Example route
-app.get("/ping", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello World!");
 });
